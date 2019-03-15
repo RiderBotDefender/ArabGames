b70d4aaa33e29c08dbbadc89f57f0e5a835822bf
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-15 14:22:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "%";
  
- 
- 
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : EX Clan`);
