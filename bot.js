69fa506f7ce5739c890755ebb0b8cff1b67ec77c
KillerPalesTine <46435288+KillerPalesTine@users.noreply.github.com>
2019-03-15 09:18:10 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,321 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const prefix = "%";
+ 
+ 
+ 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Arab Games Bot`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`%bc `,"http://twitch.tv/Death Shop")
+client.user.setStatus("online")
+});
+ 
+ 
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`عضو جديد`)
+    .setDescription(`اهلا بك في السيرفر`)
+    .addField(' 👤  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('GREEN')
+    .setFooter('The King Bot', 'https://cdn.discordapp.com/icons/390551815072251904/418fa2788d8115808951c9881ba8f190.jpg')
+ 
+var channel =member.guild.channels.find('name', 'wlc') //دا اسم روم الولكم
+if (!channel) return;
+channel.send({embed : embed});
+});
+ 
+ 
+client.on('guildMemberRemove', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`خرج عضو`)
+    .setDescription(`الى اللقاء...`)
+    .addField('👤   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('RED')
+    .setFooter(`The King Bot`, '')
+ 
+var channel =member.guild.channels.find('name', 'wlc') //دا اسم روم الولكم
+if (!channel) return;
+channel.send({embed : embed});
+});
+ 
+ 
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+ 
+    if(command === "%clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("***```ضع عدد الرسائل التي تريد مسحها 👌```***").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```php\nعدد الرسائل التي تم مسحها: " + textxt + "\n```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+ 
+ 
+ 
+ 
+ 
+var prefix = "%" ;   // البرفكس هنا
+ 
+var requestHelp = async function(type, user, message) {
+    switch(type) {
+        case "games":
+            var gamesHelp = await new Discord.RichEmbed()
+                .addField("صراحه", "لعبة صراحه")
+                .addField("حب", "لعبة حب")
+            user.send(gamesHelp);
+        break;
+        case "general":
+            var generalHelp = await new Discord.RichEmbed()
+                .addField("id", "ايديك")
+                .addField("avatar", "افتارك")
+            user.send(generalHelp);
+        break;
+        case "admin":
+        if(message.member.hasPermission("ADMINISTRATOR")) {
+            var adminHelp = await new Discord.RichEmbed()
+                .addField("clear", "مسح الشات")
+                .addField("bc", "بروكاست")
+            user.send(adminHelp);
+        } else {
+            return;
+        }
+        break;
+    }
+}
+ 
+ 
+ 
+ 
+ 
+ 
+var reactForGamesHelp = {
+    messageId: null,
+    reaction: null
+},
+reactForGeneralHelp = {
+    messageId: null,
+    reaction: null
+},
+reactForAdminHelp = {
+    messageId: null,
+    reaction: null
+};
+ 
+ 
+ 
+function define(identify) {
+    var data = {}
+    data["user"] = client.users.find("id", identify.user_id)
+    data["channel"] = client.channels.find("id", identify.channel_id);
+    data["emoji"] = identify.emoji.id ? `${identify.emoji.name}:${identify.emoji.id}` : identify.emoji.name;
+    data["member"] = data["channel"].guild.members.find("id", identify.user_id)
+    data["message"] = data["channel"].messages.find("id", identify.message_id);
+    data["reaction"] = data["message"].reactions.get(data.emoji)
+    return data;
+}
+ 
+ 
+client.on('raw',  packet  => {
+    if(packet.t == "MESSAGE_REACTION_ADD") {
+        var data = define(packet.d)
+        if(data.user.id == client.user.id) return;
+            switch (packet.d.message_id) {
+            case reactForGamesHelp.messageId:
+                if(reactForGamesHelp.reaction === data.emoji) {
+                    requestHelp("games", data.member, data.message)
+                    data.reaction.remove(data.member)
+                } else {
+                    data.reaction.remove(data.member)
+                }
+                break;
+ 
+            case reactForGeneralHelp.messageId:
+                if(reactForGeneralHelp.reaction === data.emoji) {
+                    requestHelp("general", data.member, data.message)
+                    data.reaction.remove(data.member)
+                } else {
+                    data.reaction.remove(data.member)
+                }
+                break;
+ 
+ 
+            case reactForAdminHelp.messageId:
+                if(reactForAdminHelp.reaction === data.emoji) {
+                    requestHelp("admin", data.member, data.message)
+                    data.reaction.remove(data.member)
+                } else {
+                    data.reaction.remove(data.member)
+                }
+                break;
+        }
+    }
+});
+ 
+ 
+ 
+ 
+ 
+ 
+client.on("message", message => {
+    if(message.content.indexOf(prefix) !== 0) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    if(message.content == prefix + `set ${args[1]} help`) {
+        if(args[1] == "games" || args[1] == "general" || args[1] == "admin") {
+            var  filter = m => m.author.id === message.author.id
+            message.channel.send("give me the channel id now !");        
+            message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+            .then(collected => {
+                var toSetChannel = collected.first();
+                var channel = message.guild.channels.find("id", toSetChannel.content);
+                if(channel) {
+                    message.channel.send("give me the message id now !")
+                    var  filter = m => m.author.id === message.author.id
+                    message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+                    .then(collected => {
+                        var ToSetMessage = collected.first();
+                        channel.fetchMessages().then(messages => {
+                            var defined =  messages.filter(message => message.id == ToSetMessage.content);
+                            var msg = defined.first()
+                            if(defined) {
+                                message.channel.send("send the emoji now!")
+                                message.channel.awaitMessages(filter, { max: 1, time: 40000, errors: ['time'] })
+                                .then(collected => {
+                                    msg.react(collected.first().content)
+                                    var rect = collected.first().content
+                                    setReactionData(channel, msg, rect, args[1])
+                                })
+                            }
+                        })
+                        .catch(console.error)
+                    });
+                } else {
+                    message.channel.send("sorry i can't find this channel")
+                }
+            })
+        }
+    }
+})
+var setReactionData = function(channel, message, reaction, identify) {
+    if(identify == "games") {
+        reactForGamesHelp = {
+            channel: channel,
+            messageId: message.id,
+            reaction: reaction
+        }
+    } else if(identify == "general") {
+        reactForGeneralHelp = {
+            channel: channel,
+            messageId: message.id,
+            reaction: reaction
+        }
+    } else if(identify == "admin") {
+        reactForAdminHelp = {
+            channel: channel,
+            messageId: message.id,
+            reaction: reaction
+        }
+    }
+}  
+ 
+ 
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='*member')
+      var IzRo = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURL)
+      .setFooter(message.author.username, message.author.avatarURL)
+      .setTitle('🌷| Members info')
+      .addBlankField(true)
+      .addField('📗| Online',
+      `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
+      .addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
+      .addField('📙| Idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
+      .addField('📓| Offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
+      .addField('➡| Server Members',`${message.guild.memberCount}`)
+      message.channel.send(IzRo); // كود الأعضاء الأونلاين و الأوفلاين و الخ
+   
+    });
+ 
+ 
+client.on("message", (message) => {
+ 
+   if (message.content.startsWith("%new")) {  
+        const reason = message.content.split(" ").slice(1).join(" ");  
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
+        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
+        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Support Team");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });  
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark: تم انشاء تذكرتك, #${c.name}.`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  تم انشاء تذكرتك, #ticket`)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error);
+    } // كود التيكت كامل
+ 
+  if (message.content.startsWith("%close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+ 
+      message.channel.send(`هل انت متأكد من اقفالك للتذكرة اذا متأكد اكتب.confirm`)
+          .then((m) => {
+              message.channel.awaitMessages(response => response.content === '$close', {
+                      max: 1,
+                      time: 10000,
+                      errors: ['time'],
+                  })  
+                  .then((collected) => {
+                      message.channel.delete();
+                  })  
+                  .catch(() => {
+                      m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                          m2.delete();
+                      }, 3000);
+                  });
+          });
+  }
+ 
+});
+ 
+ 
+ 
+ 
+ 
+client.login(process.env.SECRET);// سيب دي كما هي لكي لا يخرب عليك البوت
